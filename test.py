979f944173dd2b0eec0134f2f6610217355edb72
Utkarsh <utkarshsrivastava.1392006@gmail.com>
2026-06-21 12:27:29 +0530

    Keep prompting in simple req/res file

--- test.py
+++ test.py
@@ -4,22 +4,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 APIKEY=os.getenv("OpenRouterAPIKey")
-prompt = input(str("Enter prompt: "))
-response = requests.post(
-  url="https://openrouter.ai/api/v1/chat/completions",
-  headers={
-    "Authorization": f"Bearer {APIKEY}",
-    "Content-Type": "application/json",
-  },
-  data=json.dumps({
-    "model": "openrouter/owl-alpha",
-    "messages": [
-      {
-        "role": "user",
-        "content": f"{prompt}"
-      }
-    ]
-  })
-)
-data = response.json()
-print(data['choices'][0]['message']['content'])
+
+
+while True:
+  prompt = input(str("Enter prompt: "))
+  if not prompt:
+    break
+  response = requests.post(
+    url="https://openrouter.ai/api/v1/chat/completions",
+    headers={
+      "Authorization": f"Bearer {APIKEY}",
+      "Content-Type": "application/json",
+    },
+    data=json.dumps({
+      "model": "openrouter/owl-alpha",
+      "messages": [
+        {
+          "role": "user",
+          "content": f"{prompt}"
+        }
+      ]
+    })
+  )
+  data = response.json()
+  print(data['choices'][0]['message']['content'])
